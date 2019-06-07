67d29102406e6df82b4e60ba18ee7a817f1afbac
Prakash Mehta <50452488+Alvin-Mehta@users.noreply.github.com>
2019-06-07 10:33:24 +0530

    Update config.js

--- MainScreen/config.js
+++ MainScreen/config.js
@@ -1,11 +1,11 @@
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMyOk7j9yguNX055j07xdbbdMda8IKI80",
-  authDomain: "parttimer-88182.firebaseapp.com",
-  databaseURL: "https://parttimer-88182.firebaseio.com",
-  projectId: "parttimer-88182",
-  storageBucket: "parttimer-88182.appspot.com",
+  apiKey: "api key from firebase",
+  authDomain: "authorization key from firebase",
+  databaseURL: "databaseUrl from firebase",
+  projectId: "project id from firebase",
+  storageBucket: "storage bucket key from firebase",
 };
 
 let app = firebase.initializeApp(firebaseConfig);
