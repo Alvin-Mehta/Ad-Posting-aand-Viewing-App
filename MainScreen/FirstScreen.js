e6d9e03c5362d9ca40681b1114e77cac61352eaf
Prakash Mehta <50452488+Alvin-Mehta@users.noreply.github.com>
2019-06-07 10:34:29 +0530

    Update FirstScreen.js

--- MainScreen/FirstScreen.js
+++ MainScreen/FirstScreen.js
@@ -4,11 +4,11 @@ import { StyleSheet, Text,TextInput,Image,ImageBackground, View,Button,Dimension
 import * as firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMyOk7j9yguNX055j07xdbbdMda8IKI80",
-  authDomain: "parttimer-88182.firebaseapp.com",
-  databaseURL: "https://parttimer-88182.firebaseio.com",
-  projectId: "parttimer-88182",
-  storageBucket: "parttimer-88182.appspot.com",
+  apiKey: "api key from firebase",
+  authDomain: "authorization key from firebase",
+  databaseURL: "databaseUrl from firebase",
+  projectId: "project id from firebase",
+storageBucket: "storage bucket key from firebase",
 };
 
 import Icon from 'react-native-vector-icons/FontAwesome';
